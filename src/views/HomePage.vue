4c54201d3077f6d3e417988993c7cc40a8983518
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2022-08-14 14:14:17 +0800

    Create footer

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,3 +1,62 @@
 <template>
-  <main></main>
+  <div class="min-h-screen min-w-xs bg-black text-white antialiased">
+    <footer class="py-16">
+      <div class="container">
+        <div role="grid">
+          <div class="grid grid-cols-1 gap-8 md:grid-cols-3 lg:grid-cols-6" role="row">
+            <div class="md:col-span-3" role="gridcell">
+              <img class="h-24 w-auto" src="/img/premeditated-logotype.png" alt="Premeditated" />
+              <p class="text-lg text-gray-400">Complete your style with awesome Wigs from us.</p>
+              <div class="mt-4 inline-flex items-center gap-x-2">
+                <a class="flex h-8 w-8 items-center justify-center rounded-lg bg-spring" href="#" target="_blank">
+                  <span class="fab fa-facebook text-lg text-black">
+                    <span class="sr-only">Visit Facebook</span>
+                  </span>
+                </a>
+
+                <a class="flex h-8 w-8 items-center justify-center rounded-lg bg-spring" href="#" target="_blank">
+                  <span class="fab fa-instagram text-lg text-black">
+                    <span class="sr-only">Visit Instagram</span>
+                  </span>
+                </a>
+
+                <a class="flex h-8 w-8 items-center justify-center rounded-lg bg-spring" href="#" target="_blank">
+                  <span class="fab fa-twitter text-lg text-black">
+                    <span class="sr-only">Visit Twitter</span>
+                  </span>
+                </a>
+              </div>
+            </div>
+
+            <div role="gridcell">
+              <h2 class="text-xl font-bold">Company</h2>
+              <nav class="mt-4 inline-flex flex-col gap-y-4">
+                <a class="text-gray-400 hover:text-gray-300" href="#">About</a>
+                <a class="text-gray-400 hover:text-gray-300" href="#">Contact Us</a>
+                <a class="text-gray-400 hover:text-gray-300" href="#">Support</a>
+              </nav>
+            </div>
+
+            <div role="gridcell">
+              <h2 class="text-xl font-bold">Quick Links</h2>
+              <nav class="mt-4 inline-flex flex-col gap-y-4">
+                <a class="text-gray-400 hover:text-gray-300" href="#">Share Location</a>
+                <a class="text-gray-400 hover:text-gray-300" href="#">Orders Tracking</a>
+                <a class="text-gray-400 hover:text-gray-300" href="#">Size Guide</a>
+                <a class="text-gray-400 hover:text-gray-300" href="#">FAQs</a>
+              </nav>
+            </div>
+
+            <div role="gridcell">
+              <h2 class="text-xl font-bold">Legal</h2>
+              <nav class="mt-4 inline-flex flex-col gap-y-4">
+                <a class="text-gray-400 hover:text-gray-300" href="#">Terms &amp; Conditions</a>
+                <a class="text-gray-400 hover:text-gray-300" href="#">Privacy Policy</a>
+              </nav>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  </div>
 </template>
