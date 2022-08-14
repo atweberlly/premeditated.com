dc7b467a19ae566450884a0f1138ad80681793aa
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2022-08-14 21:41:05 +0800

    Import main.css

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import { createMetaManager } from 'vue-meta'
 import router from './router'
 import App from './App.vue'
+import './styles/main.css'
 
 const app = createApp(App)
 
