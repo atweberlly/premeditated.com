e1215fda6a2cb1089a0f163dc6c12de823f4dd5c
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2022-08-16 17:54:37 +0800

    Modify browserslist queries
    - Install vite-plugin-html

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,13 @@
 import { defineConfig } from 'vite'
+import { createHtmlPlugin } from 'vite-plugin-html'
 
 export default defineConfig({
+  plugins: [
+    createHtmlPlugin({
+      entry: './src/index.js',
+      minify: true,
+    }),
+  ],
   server: {
     host: '0.0.0.0',
     open: true,
