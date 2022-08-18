47482854940fbacacd9fe7ca70ccd4b0fc2d92e5
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2022-08-18 14:37:15 +0800

    Create Our Collection slider

--- src/scripts/swiper.js
+++ src/scripts/swiper.js
@@ -1,18 +1,17 @@
-import Swiper, { Navigation, Pagination } from 'swiper'
+import Swiper, { Navigation, Pagination, EffectCoverflow } from 'swiper'
 
 const mainSwiper = new Swiper('.main-swiper', {
   modules: [Navigation],
-  loop: true,
-  spaceBetween: 32,
   navigation: {
     prevEl: '.swiper-prev-button',
     nextEl: '.swiper-next-button',
   },
+  loop: true,
+  spaceBetween: 32,
 })
 
 const collectionSwiper = new Swiper('.collection-swiper', {
   modules: [Navigation, Pagination],
-
   breakpoints: {
     320: {
       slidesPerView: 1.25,
@@ -27,16 +26,47 @@ const collectionSwiper = new Swiper('.collection-swiper', {
       slidesPerView: 4,
     },
   },
-
-  centeredSlides: true,
-  loop: true,
-  spaceBetween: 32,
-
   pagination: {
     clickable: true,
     el: '.swiper-pagination',
   },
+  centeredSlides: true,
+  loop: true,
+  spaceBetween: 32,
+})
+
+const ourCollection = new Swiper('.our-collection', {
+  modules: [Navigation, EffectCoverflow],
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    640: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+  coverflowEffect: {
+    rotate: -25,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  navigation: {
+    prevEl: '.swiper-prev-button',
+    nextEl: '.swiper-next-button',
+  },
+  centeredSlides: true,
+  effect: 'coverflow',
+  grabCursor: true,
+  loop: true,
+  slidesPerView: 'auto',
+  spaceBetween: 32,
 })
 
 mainSwiper.init()
+ourCollection.init()
 collectionSwiper.init()
