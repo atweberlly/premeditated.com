2217c42330121be7e6847fa7bae0663857fcbbae
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2022-09-02 23:34:11 +0800

    Update About Us page

--- src/pages/about-us.vue
+++ src/pages/about-us.vue
@@ -18,7 +18,7 @@
           </p>
           <div class="mt-8">
             <a
-              class="inline-block rounded-lg bg-spring px-4 py-3 text-sm uppercase tracking-wide text-black"
+              class="inline-block rounded bg-spring px-4 py-3 text-sm font-medium uppercase tracking-wide text-black"
               href="#"
             >
               View Collections
@@ -28,6 +28,91 @@
 
         <div class="md:w-6/12">
           <img class="ml-16 md:ml-0" src="/img/about-us-hero.png" alt="" />
+        </div>
+      </div>
+    </section>
+
+    <section
+      class="mx-auto max-w-6xl overflow-x-hidden px-8 py-8 sm:py-12 md:py-16 lg:py-24 xl:px-0 xl:py-32"
+    >
+      <div
+        class="flex flex-col border border-r-8 border-gray-700 border-r-[#5f0074] bg-gradient-to-br from-white/20 supports-backdrop-blur:backdrop-blur-lg md:flex-row md:items-center md:py-8 lg:pt-0 xl:pb-16"
+      >
+        <div class="mx-4 my-4 md:my-0 md:mr-0 md:w-6/12 lg:-mt-16 lg:ml-8 xl:-mt-24 xl:ml-16">
+          <img class="border-8 border-gray-800" src="/img/our-mission.jpg" alt="" />
+        </div>
+        <div class="border-y border-white px-4 py-4 md:w-6/12 lg:px-8 lg:py-8 xl:p-12">
+          <h2 class="font-display text-2xl tracking-widest text-spring xl:text-3xl">Our Mission</h2>
+          <div class="mt-4 space-y-4 text-gray-400">
+            <p>To provide vibrant, high-quality wigs for anyone who wants to change their look.</p>
+            <p>
+              We want to make sure that everyone can have the hair they've always wanted without
+              damaging their hair or spending a lot of time styling. Our wigs come pre-styled in
+              various colors so that you can find the perfect wig for you.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section
+      class="mx-auto max-w-6xl overflow-x-hidden px-8 py-8 sm:py-12 md:py-16 lg:py-24 xl:px-0 xl:py-32"
+    >
+      <div
+        class="flex flex-col border border-l-8 border-gray-700 border-l-[#5f0074] bg-gradient-to-bl from-white/20 supports-backdrop-blur:backdrop-blur-lg md:flex-row md:items-center md:py-8 lg:pt-0 xl:pb-16"
+      >
+        <div
+          class="mx-4 my-4 md:order-2 md:my-0 md:ml-0 md:w-6/12 lg:-mt-16 lg:mr-8 xl:-mt-24 xl:mr-16"
+        >
+          <img class="border-8 border-gray-800" src="/img/our-team.jpg" alt="" />
+        </div>
+        <div
+          class="border-y border-gray-700 px-4 py-4 md:order-1 md:w-6/12 lg:px-8 lg:py-8 xl:p-12"
+        >
+          <h2 class="font-display text-2xl tracking-widest text-spring xl:text-3xl">Our Team</h2>
+          <div class="mt-4 space-y-4 text-gray-400">
+            <p>
+              Our team has a wealth of experience in the beauty industry and works to provide the
+              best customer service.
+            </p>
+            <p>
+              We want to ensure that you are happy with your wig, no matter your style. We offer a
+              money-back guarantee on all of our products, so you can be sure that you're getting
+              the best possible value for your money.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section
+      class="mx-auto max-w-6xl overflow-x-hidden px-8 py-8 sm:py-12 md:py-16 lg:py-24 xl:px-0 xl:py-32"
+    >
+      <div class="flex flex-col gap-8 md:flex-row md:items-center xl:gap-16">
+        <div class="md:order-2 md:w-6/12">
+          <h2 class="text-3xl uppercase leading-tight tracking-tight lg:text-4xl lg:leading-tight">
+            Looking for a new way to express yourself?
+          </h2>
+          <p class="mt-4 uppercase leading-relaxed text-gray-400 lg:text-xl lg:leading-relaxed">
+            <span class="font-semibold text-purple">Premeditated Beauty</span> is the perfect place
+            for you!
+          </p>
+          <p class="mt-4 leading-relaxed text-gray-400 lg:text-xl lg:leading-relaxed">
+            We offer a wide variety of styles and colors so that you can find the perfect wig for
+            your needs!
+          </p>
+          <div class="mt-8">
+            <a
+              class="inline-block rounded bg-spring px-4 py-3 text-sm font-medium uppercase tracking-wide text-black"
+              href="#"
+            >
+              Shop now
+            </a>
+          </div>
+        </div>
+
+        <div class="md:order-1 md:w-6/12">
+          <img class="-ml-16 md:ml-0" src="/img/image.png" alt="" />
         </div>
       </div>
     </section>
