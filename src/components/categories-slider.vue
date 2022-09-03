9552abc0c4c34b64de0a3217f77047b00bab4a7a
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2022-09-03 17:06:09 +0800

    Create categories-slider.vue

---
+++ src/components/categories-slider.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex flex-row items-center gap-x-4 overflow-x-auto px-4 py-4 lg:hidden">
+    <a class="shrink-0" v-for="i in 16" :key="i" href="#">
+      <!-- <img
+        class="h-16 w-16 rounded-full"
+        src=""
+        alt="Category"
+      /> -->
+      <div class="h-16 w-16 rounded-full bg-gray-500"></div>
+      <div class="mt-2 text-xs uppercase tracking-wide">Category</div>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    document.title = 'Categories Slider'
+  },
+}
+</script>
